eca7f81cd07a67ab03cb3684128e3967a28e1a85
yacine-boucetta <yacine.boucetta@laplateforme.io>
2022-02-09 09:20:44 +0100

    router en cours

--- index.php
+++ index.php
@@ -2,13 +2,5 @@
 
 $url = '';
 
-if (isset($_GET['url'])) {
-    $url = explode('/', $_GET['url']);
-}
 
-var_dump($url);
-if ($url == '') {
-    echo "Page d'acceuil";
-} elseif ($url[0] == 'article' and !empty($url[1])) {
-    echo 'article numero' . $url[1];
-}
+
